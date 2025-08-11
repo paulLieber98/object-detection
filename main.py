70dc32e9e7a3e641d17864f78dbdb0b3d53d57bf
paulLieber98 <paullieber07@gmail.com>
2025-08-10 22:11:25 -0700

    yolov8 model works + camera

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 import inference
 import cv2 as cv
+from ultralytics import YOLO #for model
 
- 
-webcam = cv.VideoCapture(1)
+#DEFINING MODEL
+model = YOLO("yolov8n.pt")
 
-if not webcam.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = webcam.read()
+#LOADING MODEL
+model.load("yolov8n.pt")
+
+results = model.predict(source=1, show=True)
+
+# #WEBCAM OPENCV
+# webcam = cv.VideoCapture(1)
+
+# if not webcam.isOpened():
+#     print("Cannot open camera")
+#     exit()
+# while True:
+#     # Capture frame-by-frame
+#     ret, frame = webcam.read()
  
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    #flip so not inverted (dont know why it does this by default)
-    frame = cv.flip(frame, 1)
-    # Display the resulting frame
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
+#     # if frame is read correctly ret is True
+#     if not ret:
+#         print("Can't receive frame (stream end?). Exiting ...")
+#         break
+#     #flip so not inverted (dont know why it does this by default)
+#     frame = cv.flip(frame, 1)
+#     # Display the resulting frame
+#     cv.imshow('frame', frame)
+#     if cv.waitKey(1) == ord('q'):
+#         break
  
-# When everything done, release the capture
-webcam.release()
-cv.destroyAllWindows()
+# # When everything done, release the capture
+# webcam.release()
+# cv.destroyAllWindows()
