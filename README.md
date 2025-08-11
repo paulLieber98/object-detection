fbe6e48f1a725c89b3491417562a0e288e31d03e
paulLieber98 <paullieber07@gmail.com>
2025-08-11 13:17:36 -0700

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Practicing computer vision: trying out Yolov8 models for object detection + labelling. Using opencv for webcam. 
+Practicing computer vision: trying out Yolov8 models for object detection + labelling.
 
 model: https://roboflow.com/model/yolov8
+
+GOAL: using IDs to track if same person. mp3 ding goes off if someone else enters room via camera in corner. Using same model
--- main.py
+++ main.py
@@ -8,29 +8,8 @@ model = YOLO("yolov8n.pt")
 #LOADING MODEL
 model.load("yolov8n.pt")
 
-results = model.predict(source=1, show=True)
+# results = model.predict(source=1, show=True) #predict: track just no IDs
+results = model.track(source=1, show=True) #track: shows IDs
 
-# #WEBCAM OPENCV
-# webcam = cv.VideoCapture(1)
 
-# if not webcam.isOpened():
-#     print("Cannot open camera")
-#     exit()
-# while True:
-#     # Capture frame-by-frame
-#     ret, frame = webcam.read()
- 
-#     # if frame is read correctly ret is True
-#     if not ret:
-#         print("Can't receive frame (stream end?). Exiting ...")
-#         break
-#     #flip so not inverted (dont know why it does this by default)
-#     frame = cv.flip(frame, 1)
-#     # Display the resulting frame
-#     cv.imshow('frame', frame)
-#     if cv.waitKey(1) == ord('q'):
-#         break
- 
-# # When everything done, release the capture
-# webcam.release()
-# cv.destroyAllWindows()
+
