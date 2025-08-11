2b37d3bb3617d09f0b11eba1c52cf42db607a470
paulLieber98 <paullieber07@gmail.com>
2025-08-10 21:51:20 -0700

    added opencv webcam opening

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Practicing computer vision: trying out Yolov8 models for object detection + labelling. Using opencv for webcam. 
+
+model: https://roboflow.com/model/yolov8
--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import inference
 import cv2 as cv
 
+ 
+webcam = cv.VideoCapture(1)
+
+if not webcam.isOpened():
+    print("Cannot open camera")
+    exit()
+while True:
+    # Capture frame-by-frame
+    ret, frame = webcam.read()
+ 
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    #flip so not inverted (dont know why it does this by default)
+    frame = cv.flip(frame, 1)
+    # Display the resulting frame
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+webcam.release()
+cv.destroyAllWindows()
